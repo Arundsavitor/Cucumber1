65a87db2c3cd6fad58cce9faa110262c5c8e833c
training_d2.03.07 <training_d2.03.07@DDC2-D-6NNR9B2.dir.svc.accenture.com>
2019-07-03 10:20:28 +0530

    Edited

--- src/test/java/Calc/CalcRun.java
+++ src/test/java/Calc/CalcRun.java
@@ -2,9 +2,12 @@ package Calc;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin= {"json:cucumber.json"})
 public class CalcRun {
 
 }
